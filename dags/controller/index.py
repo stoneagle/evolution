2c1069afc7a5fa9529f54faab0c7882155865c8a
wuzhongyang <b40750428@hotmail.com>
2017-11-30 06:53:33 +0800

    完成价格所处均线位置的计算

--- dags/controller/index.py
+++ dags/controller/index.py
@@ -42,11 +42,61 @@ def one_index(f, code, ktype, start_date):
     df = df.set_index(conf.HDF5_SHARE_DATE_INDEX)
     # 计算macd
     macd = talib.MACD(df["close"].values, fastperiod=12, slowperiod=26, signalperiod=9)
+
     # 计算均值
+    tmp_mean_dict = dict()
+    for i in range(0, 481):
+        tmp_mean_dict[i] = df["close"].rolling(window=i).mean()
+
     df_index = pd.DataFrame(np.column_stack(macd), index=df.index, columns=conf.HDF5_INDEX_COLUMN)
+    df_index["close"] = df["close"]
     df_index["macd"] = df_index["macd"] * 2
     for i in [5, 10, 30, 60, 240, 480, 365]:
-        df_index["ma_" + str(i)] = df["close"].rolling(window=i).mean()
+        df_index["ma_" + str(i)] = tmp_mean_dict[i]
     if start_date is not None:
         df_index = df_index.ix[start_date:]
+
+    # 计算close价格所处均线位置
+    for index, row in df_index.iterrows():
+        # 先确认均线是上行还是下行
+        ma5_price = row["ma_5"]
+        ma10_price = row["ma_10"]
+        if np.isnan(ma5_price) or np.isnan(ma10_price):
+            df_index.loc[index, "ma_border"] = np.NaN
+            continue
+
+        if ma5_price >= ma10_price:
+            up_flag = True
+        else:
+            up_flag = False
+
+        border = 0
+        # 1. 如果下行，从小于找起直到出现大于;如果上行，从大于找起直到出现小于
+        for i in [30, 60, 240, 480]:
+            compare_price = row["ma_" + str(i)]
+            if compare_price is np.NaN:
+                break
+
+            if (ma5_price > compare_price and up_flag is False) or (ma5_price < compare_price and up_flag is True):
+                border = i
+                break
+
+        # 2. 根据边界均线，按比例调整，找出离close最贴近的均线
+        if border != 0:
+            border_price = row["ma_" + str(border)]
+            df_index.loc[index, "ma_border"] = border
+            min_diff = 0
+            min_diff_ma_num = 0
+            if (up_flag is False and row["close"] > border_price) or (up_flag is True and row["close"] < border_price):
+                for i in range(0, border - 5):
+                    tmp_mean = tmp_mean_dict[border - i]
+                    dynamic_price = tmp_mean.loc[index, ]
+
+                    # 寻找close最贴近的均线
+                    if min_diff == 0 or abs(row["close"] - dynamic_price) <= min_diff:
+                        min_diff = abs(row["close"] - dynamic_price)
+                        min_diff_ma_num = border - i
+                df_index.loc[index, "ma_border"] = min_diff_ma_num
+        else:
+            df_index.loc[index, "ma_border"] = 5
     return df_index
