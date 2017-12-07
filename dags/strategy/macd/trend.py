8dcf7f9a4430741380a60ac8768d7aa3db6b6c3d
wuzhongyang <b40750428@hotmail.com>
2017-12-07 20:10:30 +0800

    调整phase的内容与结构

--- dags/strategy/macd/trend.py
+++ dags/strategy/macd/trend.py
@@ -1,28 +1,13 @@
-from strategy.phase import phase, action
+from strategy.phase import phase
 
 
-def get_macd_trends(index_df):
+def get_macd_phases(index_df, trend_df):
     """
-    获取某个时间段的macd分布情况
-    """
-    index_df = index_df.reset_index()
-    first = index_df.iloc[1]["macd"]
-    second = index_df.iloc[2]["macd"]
-    start_date = index_df.iloc[1]["date"]
-    phase_obj = phase.Phase(first, second, start_date)
-    action_machine = action.Action(phase_obj)
-
-    for index, row in index_df[3:].iterrows():
-        action_machine.run(row["macd"], row["date"])
-    return action_machine.phase.result()
-
-
-def get_price_relate(trend_df):
-    """
-    根据阶段时间范围，获取以下数值
+    获取macd的阶段情况
     记录各个红柱、绿柱面积
     记录各个红柱、绿柱面积范围的价格变化
     比较前后两段红柱/绿柱的价格高点/低点，判断趋势方向(上行、下行、震荡)
     比较两段绿柱，价格的波动幅度
     """
-    return
+    phase_obj = phase.Phase()
+    return phase_obj
---
+++ dags/strategy/util/phase.py
@@ -0,0 +1,9 @@
+class Phase(object):
+    # trend数据
+    trend_df = None
+    # 阶段聚合结果
+    phase_dict = dict()
+
+    def __init__(self, trend_df):
+        self.trend_df
+        return
