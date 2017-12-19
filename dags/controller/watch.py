f09d27bb11455aacb603ef74e40a5951e30cc0d1
wuzhongyang <b40750428@hotmail.com>
2017-12-19 19:33:09 +0800

    支持中途监听时，复现当天买卖点

--- dags/controller/watch.py
+++ dags/controller/watch.py
@@ -8,6 +8,7 @@ import threading
 # 定时器
 timer = None
 share_dict = dict()
+init_flag = True
 
 
 def tushare(code_list):
@@ -33,16 +34,6 @@ def tushare(code_list):
             # share_dict[code][ktype] = share_df.head(len(share_df) - 48)
             # share_dict[code][ktype] = share_dict[code][ktype].tail(60)
             share_dict[code][ktype] = share_df.tail(60)
-            index_df = macd.value(share_dict[code][ktype])
-            share_df = share_df.merge(index_df, left_index=True, right_index=True, how='left')
-            share_df = share_df.dropna()
-            trend_df = macd.trend(share_df.reset_index())
-            trend_df = trend_df[["date", "turn_count", "trend_count", "status", "phase_status", "macd"]].tail(48)
-            for i in range(2, len(trend_df)):
-                tmp_df = trend_df.head(i)
-                now = tmp_df.iloc[-1]
-                pre = tmp_df.iloc[-2]
-                _check(code, pre, now)
     f.close()
     daily()
     # bot = weixin.WXBot()
@@ -52,6 +43,7 @@ def tushare(code_list):
 
 def daily():
     global share_dict
+    global init_flag
     # 监听仓位中的股票，发出5min和30min的出入信号
     for code, data in share_dict.items():
         for ktype, share_df in data.items():
@@ -63,6 +55,7 @@ def daily():
                     time.sleep(30)
                 else:
                     break
+
             today_df = today_df.tail(len(today_df) - 1)
             today_df = today_df.set_index(conf.HDF5_SHARE_DATE_INDEX)
             share_df = share_df.append(today_df)
@@ -70,10 +63,20 @@ def daily():
             share_df = share_df.merge(index_df, left_index=True, right_index=True, how='left')
             share_df = share_df.dropna()
             trend_df = macd.trend(share_df.reset_index())
-            trend_df = trend_df[["date", "turn_count", "trend_count", "status", "phase_status", "macd"]].tail(48)
-            now = trend_df.iloc[-1]
-            pre = trend_df.iloc[-2]
-            _check(code, pre, now)
+            trend_df = trend_df[["date", "turn_count", "trend_count", "status", "phase_status", "macd", "dif", "dea"]].tail(48)
+
+            # 当天第一次开启监控时，读取数据并初始化
+            if init_flag is True:
+                for i in range(2, len(trend_df)):
+                    tmp_df = trend_df.head(i)
+                    now = tmp_df.iloc[-1]
+                    pre = tmp_df.iloc[-2]
+                    _check(code, pre, now)
+                init_flag = False
+            else:
+                now = trend_df.iloc[-1]
+                pre = trend_df.iloc[-2]
+                _check(code, pre, now)
     # TODO 监听筛选池的股票，发出5min和30min的出入信号
 
     global timer
@@ -86,8 +89,10 @@ def _check(code, pre, now):
     # 判断第一次出现的shake
     if pre["trend_count"] >= 4 and now["status"] == action.STATUS_SHAKE and abs(pre["macd"]) >= 0.01:
         if pre["status"] == action.STATUS_UP:
+            # dif与dea线，都在0轴以上0.01才算
             msg = "%s在%s出现5min卖点,连续趋势%d次,macd差值%f"
         elif pre["status"] == action.STATUS_DOWN:
+            # dif与dea线，都在0轴以下0.01才算
             msg = "%s在%s出现5min买点,连续趋势%d次,macd差值%f"
         console.write_msg(msg % (code, now["date"], pre["trend_count"], now["macd"] - pre["macd"]))
     return
