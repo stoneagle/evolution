b273fc14f336f6f34c04cd75ed8885059849dae2
wuzhongyang <wuzhongyang@roobo.com>
2018-06-10 17:58:08 +0800

    完成thrift的新增部署

---
+++ hack/thrift/engine.thrift
@@ -0,0 +1,25 @@
+namespace go engine 
+namespace py engine 
+
+enum QuantType {
+    Stock = 1,
+    Exchange = 2
+}
+
+enum ResponseState {
+    StateOk = 0,
+    StateErrorBusiness = 1,
+    StateErrorEmpty = 2
+}
+
+struct Response {
+    1: ResponseState code
+    2: string desc
+    3: string data
+}
+
+service EngineService {
+    Response getType(1: QuantType resource);
+    Response getStrategy(1: string stype);
+    Response getClassify(1: string stype, 2: string source);
+}
